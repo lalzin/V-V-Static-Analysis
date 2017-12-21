73a66f55c87338a1924d18d3ad2abb749843170a
Unknown <alanmarzin@gmail.com>
2017-12-21 12:22:02 +0100

    Auto stash before merge of "master" and "origin/master"

--- src/main/java/istic/vv/CycloProcessor.java
+++ src/main/java/istic/vv/CycloProcessor.java
@@ -9,19 +9,9 @@ import org.apache.commons.io.filefilter.DirectoryFileFilter;
 import org.apache.commons.io.filefilter.RegexFileFilter;
 
 import spoon.processing.AbstractProcessor;
-import spoon.reflect.code.CtBlock;
-import spoon.reflect.code.CtConditional;
 import spoon.reflect.code.CtIf;
 import spoon.reflect.code.CtLoop;
-import spoon.reflect.code.CtStatement;
-import spoon.reflect.declaration.CtMethod;
-import spoon.reflect.visitor.Filter;
 import spoon.reflect.visitor.filter.TypeFilter;
-import spoon.support.reflect.code.CtForEachImpl;
-import spoon.support.reflect.code.CtForImpl;
-import spoon.support.reflect.code.CtIfImpl;
-import spoon.support.reflect.code.CtSwitchImpl;
-import spoon.support.reflect.code.CtWhileImpl;
 import spoon.support.reflect.declaration.CtMethodImpl;
 
 public class CycloProcessor extends AbstractProcessor<CtMethodImpl> {
